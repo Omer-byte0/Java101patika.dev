24426f5e78c00b193e21a69d65540236144988b9
Ömer Faruk Kaya <81438463+Omer-byte0@users.noreply.github.com>
2021-06-18 21:53:19 +0300

    Add files via upload

---
+++ Java101/Odev9/src/Odev9/Odev9.java
@@ -0,0 +1,75 @@
+package Odev9;
+import java.util.Scanner;
+
+public class Odev9 {
+	public static void main(String[] args) {
+		
+		String userName, password;
+		String passwordLogin="1234";
+		Scanner inp = new Scanner(System.in);
+		
+		System.out.print("Kullanici Adinizi Giriniz:");
+		userName = inp.nextLine();
+		
+		System.out.print("Sifrenizi Giriniz:");
+		password = inp.nextLine();
+		
+		if(userName.equals("Omer") && password.equals(passwordLogin)){
+			System.out.print("Giris Basarili.");
+		}
+		else {
+			
+			System.out.println("Giris Basarisiz.");
+			System.out.println("Sifrenizi sifirlamak ister misiniz? ");
+			System.out.println("Evet icin 1, Hayir icin 2 giriniz.");
+			
+			int deger = inp.nextInt();
+			
+			switch(deger) {
+			
+			case 1:
+				System.out.println("Yeni sifre olusturunuz:");
+				String newPassword = inp.next();// Esra arkadaþýmýn ödevinden aldým. Veritipi olarak tanýmlamayarak hem string hem de int olarak veri atayabilmek için.
+												// Ayrýca if döngüsünde int veri tipiyle stringi ayný anda kabul etmiyor.
+				
+				if(newPassword.equals(passwordLogin) || newPassword.equals(password)) {
+					
+					System.out.print("Eski sifrenizle ayni sifreyi koyamazsiniz.");
+					}
+				
+				else {
+					System.out.print("Yeni sifre olusturuldu.");
+					password = newPassword;
+					}
+				
+				break;
+			
+			case 2:
+				
+				System.out.print("Sifre sifirlama iptal edildi.");
+				
+				break;
+				
+				default:
+					System.out.print("Ýstenen degerlerden birini seciniz.");
+							
+						}
+						
+						
+						
+
+					}
+					
+				}
+
+			
+			
+
+			
+
+
+		}
+		
+	
+
+
